76f4ed809cbdef0f0575bff87809272f9f20407b
Mario Pietsch <pmariojo@gmail.com>
2019-02-10 16:31:23 +0100

    WIP but looks good

---
+++ wikilabs/uni-link/tiddlers/make-alias-link.js
@@ -0,0 +1,29 @@
+/*\
+title: $:/plugins/wikilabs/uni-link/make-alias-link.js
+type: application/javascript
+module-type: texteditoroperation
+
+Text editor operation to make an alias-link
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+exports["make-alias-link"] = function(event,operation) {
+	if(operation.selection) {
+		operation.replacement = "[[" + operation.selection + "|?" + event.paramObject.text + "]]";
+		operation.cutStart = operation.selStart;
+		operation.cutEnd = operation.selEnd;
+	} else {
+		operation.replacement = "[[" + event.paramObject.text + "|?]]";
+		operation.cutStart = operation.selStart;
+		operation.cutEnd = operation.selEnd;
+	}
+	operation.newSelStart = operation.selStart + operation.replacement.length;
+	operation.newSelEnd = operation.newSelStart;
+};
+
+})();
