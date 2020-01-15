3b491941cdddd45b2d35be3d1dd3ea02e3e9362f
Mario Pietsch <pmariojo@gmail.com>
2020-01-15 16:09:55 +0100

    make tm-open-story paramObjects aware, so we get additional info.

--- wikilabs/wltm-open-story/tiddlers/open-story.js
+++ wikilabs/wltm-open-story/tiddlers/open-story.js
@@ -9,7 +9,7 @@ A startup module
 (function(){
 
 /*jslint node: true, browser: true */
-/*global $tw: false */
+/*global $tw: false, exports:false, window:false*/
 "use strict";
 
 // Export name and synchronous status
@@ -25,7 +25,7 @@ var DEFAULT_HISTORY_TITLE = "$:/HistoryList";
 // Default tiddlers
 var DEFAULT_TIDDLERS_TITLE = "$:/DefaultTiddlers";
 
-	/*
+/*
 Get the value of a text reference. Text references can have any of these forms:
 	<tiddlertitle>
 	<tiddlertitle>!!<fieldname>
@@ -39,10 +39,9 @@ exports.startup = function() {
 		$tw.rootWidget.addEventListener("tm-open-story",function(event) {
 			window.location.hash = "";
 			var storyTitle = event.param ? event.param : DEFAULT_TIDDLERS_TITLE;
-
-			var storyFilter = $tw.wiki.getTextReference(storyTitle, "GettingStarted"),
-				storyList = $tw.wiki.filterTiddlers(storyFilter),
-				options = event.paramObject || {};
+			var field = (event.paramObject && event.paramObject.field) ? event.paramObject.field : "list";
+			var storyFilter = $tw.wiki.getTextReference(storyTitle + "!!" + field, "GettingStarted!!text");
+			var storyList = $tw.wiki.filterTiddlers(storyFilter);
 			//invoke any hooks that might change the default story list
 			storyList = $tw.hooks.invokeHook("th-opening-story",storyList);
 			$tw.wiki.addTiddler({title: DEFAULT_STORY_TITLE, text: "", list: storyList},$tw.wiki.getModificationFields());
