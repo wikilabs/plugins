bf5e24bead68ab4768e056d102e2522d34c1d00f
Mario Pietsch <pmariojo@gmail.com>
2020-05-25 09:32:28 +0200

    prepare for library edition and fix uni-link

--- wikilabs/uni-link/tiddlers/modules/wiki-methodes.js
+++ wikilabs/uni-link/tiddlers/modules/wiki-methodes.js
@@ -19,10 +19,41 @@ exports.getTiddlerAliasBacklinks = function(targetTitle) {
 
 //var widget = require("$:/core/modules/widgets/widget.js");
 
+
+/*
+Return an array of tiddler titles that are directly linked within the given parse tree
+ */
+exports.extractLinks = function(parseTreeRoot) {
+	// Count up the links
+	var links = [],
+		checkParseTree = function(parseTree) {
+			for(var t=0; t<parseTree.length; t++) {
+				var parseTreeNode = parseTree[t];
+				if(parseTreeNode.type === "link" && parseTreeNode.attributes.to && parseTreeNode.attributes.to.type === "string") {
+					var value = parseTreeNode.attributes.to.value;
+					if(links.indexOf(value) === -1) {
+						links.push(value);
+					}
+				} else if (parseTreeNode.type === "macrocall" && parseTreeNode.name === "uni-link" && parseTreeNode.params && parseTreeNode.params[0].value) {
+					var value = parseTreeNode.params[0].value;
+					if(links.indexOf(value) === -1) {
+						links.push(value);
+					}
+				} // else if type==="macrocall"
+
+				if(parseTreeNode.children) {
+					checkParseTree(parseTreeNode.children);
+				}
+			}
+		};
+	checkParseTree(parseTreeRoot);
+	return links;
+};
+
 /*
 Return an array of tiddler titles that are directly linked from the specified tiddler
 */
-exports.getTiddlerLinks = function(title) {
+exports.getTiddlerLinksXXX = function(title) {
 	var self = this;
 	// We'll cache the links so they only get computed if the tiddler changes
 	return this.getCacheForTiddler(title,"links",function() {
@@ -43,7 +74,6 @@ exports.getTiddlerLinks = function(title) {
 						if(links.indexOf(value) === -1) {
 							links.push(value);
 						}
-
 					} // else if type==="macrocall"
 
 					if(parseTreeNode.children) {
