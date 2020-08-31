7e961751cef95f23868486c6ab498051827c594f
Mario Pietsch <pmariojo@gmail.com>
2020-08-31 20:26:13 +0200

    WIP works with HR as an end marker - BLOCK mode

--- wikilabs/tick-text/tiddlers/wikirules/ticktext.js
+++ wikilabs/tick-text/tiddlers/wikirules/ticktext.js
@@ -21,11 +21,11 @@ exports.types = {block: true};
 exports.init = function(parser) {
 	this.parser = parser;
 	// Regexp to match
-//	this.matchRegExp = /(\´{1,3})/mg; //a  OK
+	this.matchRegExp = /(\´{1,4})/mg; //a  OK
 //	this.matchRegExp = /(\.{1,3})/mg; //b  CSS interference
 //	this.matchRegExp = /(\. )/mg;     //y  OK see: dot-space!
 //	this.matchRegExp = /(\´)(\t{1,2})?/mg; //x  OK
-	this.matchRegExp = /(\´)(\´{1,3})?/mg; //x  OK
+//	this.matchRegExp = /(\´)(\´{1,3})?/mg; //x  OK
 //	this.matchRegExp = /(\. )|(\.{2,4} )/mg; //y  OK
 };
 
@@ -34,16 +34,19 @@ Parse the most recent match
 */
 exports.parse = function() {
 	// Get all the details of the match
-//	var level = this.match[1].length; //abc
-	var level = (this.match[2]) ? this.match[2].length + 1 : 1; //x
+	var level = this.match[1].length; //abc
+//	var level = (this.match[2]) ? this.match[2].length + 1 : 1; //x
 //	var level = (this.match[2]) ? this.match[2].length - 1 : 1; //y
 	// Move past the !s
 	this.parser.pos = this.matchRegExp.lastIndex;
 	// Parse any classes, whitespace and then the heading itself
 	var classes = this.parser.parseClasses();
 	classes.push(CLASS_PREFIX + level + " " + CLASS_GROUP);
-	this.parser.skipWhitespace({treatNewlinesAsNonWhitespace: true});
-	var tree = this.parser.parseInlineRun(/(\r?\n)/mg);
+	this.parser.skipWhitespace();
+//	this.parser.skipWhitespace({treatNewlinesAsNonWhitespace: true});
+//	var tree = this.parser.parseInlineRun(/(\r?\n)/mg); // OK for single new-line
+//	var tree = this.parser.parseInlineRun(/(^---)/mg,{eatTerminator:true}); // geht nicht für blöcke
+	var tree = this.parser.parseBlocks("^---+$");  // wip OK
 	// Return the paragraph
 	return [{
 		type: "element",
