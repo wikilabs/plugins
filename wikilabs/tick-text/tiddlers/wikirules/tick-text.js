9df8857d0783fbd16244914069a4bd5c6889283f
Mario Pietsch <pmariojo@gmail.com>
2020-09-07 17:09:43 +0200

    WIP - want to test ° instead of tick

--- wikilabs/tick-text/tiddlers/wikirules/tick-text.js
+++ wikilabs/tick-text/tiddlers/wikirules/tick-text.js
@@ -5,6 +5,13 @@ module-type: wikirule
 
 Wiki text block rule for ticktext and angeltext
 
+Detect
+
+´´´asdf.my.Class This is some text with a name and class
+´.a.b.c.d This is some text with class
+»»»asdf.my.Class This is some text with a name and class
+».a.b.c.d This is some text with class
+
 \*/
 (function(){
 
@@ -20,11 +27,17 @@ exports.types = {block: true};
 
 exports.init = function(parser) {
 	this.parser = parser;
-	// Regexp to match
-	this.matchRegExp = /(^\´{1,4}|^»{1,4})/mg; //a  OK
-
+	// Regexp to match 
+//	this.matchRegExp = /(^´{1,4}|^»{1,4})/mg; //a  OK
+	
+	// match[1] ... all symbols 1-4 ´ or »
+	// match[2] ... tagName .... HTML tag default DIV
+	// match[3] ... classString
+//	this.matchRegExp = /(^´{1,4}|^»{1,4})((?:[^\.\r\n\s]+))?(\.(?:[^\r\n\s]+))?/mg; //a  OK
+	this.matchRegExp = /((?=´[^´])´|»{1,4})((?:[^\.\r\n\s´]+))?(\.(?:[^\r\n\s]+))?/mg; //a  OK
+	
 	this.p = this.parser;
-	this.p.configTickText = this.p.configTickText  || {};
+	this.p.configTickText = this.p.configTickText || {};
 	
 	this.pc = this.p.configTickText;
 	this.pc.tick = this.pc.tick || {};
@@ -35,33 +48,35 @@ exports.init = function(parser) {
 Parse the most recent match
 */
 exports.parse = function() {
-	var self = this,
-		tree;
-	
-	// Get all the details of the match
-	var level = this.match[1].length; //abc
-	var id = (this.match[1][0] === "´") ? "tick" : (this.match[1][0] === "»") ? "angel" : "x";
-
 	/*
 	Skip the endstring at the current position. Options are:
 	treatNewlinesAsNonWhitespace: true if newlines are NOT to be treated as whitespace
 	*/
-	var skipEndString = function(endString) {
+	function skipEndString (endString) {
 		var endRegExp = new RegExp("(" + $tw.utils.escapeRegExp(endString) + ")","mg")
 		endRegExp.lastIndex = self.parser.pos;
 		var endMatch = endRegExp.exec(self.parser.source);
 		if(endMatch && endMatch.index === self.parser.pos) {
 			self.parser.pos = endRegExp.lastIndex;
 		}
-	};
+	}
+
+//---------------------
+	var self = this,
+		tree;
+
+	// Get all the details of the match
+	var level   = this.match[1].length; //abc
+	var id      = (this.match[1][0] === "´") ? "tick" : (this.match[1][0] === "»") ? "angel" : null;
+	var sym     = this.match[2]; // needs to be undefined if no match
+	var params  = (this.match[3]) ? this.match[3] : "";
+
+	var options = {symbol: sym, mode : "inline", tag : (id==="tick") ? "div" : "p", params : params, endString : "", use: ""};
 	
-	var options = {symbol: "¤", mode : "inline", tag : (id==="tick") ? "div" : "p", params : "", endString : "", use: ""};
-	
-	// Move past the !s
+	// Move past the start symbol
 	this.parser.pos = this.matchRegExp.lastIndex;
 	// Parse any classes, whitespace and then the heading itself
-	var classes = this.parser.parseClasses();
-	var sym = classes[0];
+	var classes = params.split(".");
 
 	if (!sym && this.pc[id]["true"]) {
 		sym = this.pc[id]["true"].use;
@@ -74,8 +89,9 @@ exports.parse = function() {
 		options.endString = this.pc[id][sym].endString || options.endString;
 		options.mode = this.pc[id][sym].mode || options.mode;
 		options.tag = this.pc[id][sym].tag || options.tag;
-		options.params = this.pc[id][sym].params.split(".").join(" ").trim() || options.params;
-		classes[0] = options.params // replace the name element
+		options.params = this.pc[id][sym].params || options.params;
+		classes = options.params.split(".") // pragma options win????????????????????
+//		classes[0] = options.params.split(".").join(" ").trim() // replace the name element
 	}
 	
 //	if (id === "tick") {
@@ -105,7 +121,7 @@ exports.parse = function() {
 	var fixAttributes = ["tick", "angel", "symbol", "endString", "mode", "tag", "params", "use"];
 	
 	var attributes = {
-			"class": {type: "string", value: classes.join(" ")}
+			"class": {type: "string", value: classes.join(" ").trim()}
 		}
 	// Callback is invoked with (element,title,object)
 	$tw.utils.each(this.pc[id][sym], function(val,title,ob) {
