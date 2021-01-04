fcaafab53861a34de85c05fef4e345d9cda26b04
Mario Pietsch <pmariojo@gmail.com>
2021-01-04 21:05:19 +0100

    should improve compatibility with other plugins

---
+++ wikilabs/click-effect/tiddlers/core/dom-tweaks.js
@@ -0,0 +1,57 @@
+/*\
+title: $:/core/modules/utils/dom-tweaks.js
+type: application/javascript
+module-type: utils
+
+Fix .pulseElement()
+Add . clcikAnimation()
+
+Various static DOM-related utility functions.
+\*/
+
+(function(){
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+FIX - FIX - FIX - FIX - FIX - FIX - should be part of the core
+Pulse an element for debugging purposes
+*/
+exports.pulseElement = function(element) {
+	var eventName = $tw.utils.convertEventName("animationEnd");
+	// Event handler to remove the class at the end
+	element.addEventListener(eventName,function handler(event) {
+		element.removeEventListener(eventName,handler,false);
+		$tw.utils.removeClass(element,"pulse");
+	},false);
+	// Apply the pulse class
+	$tw.utils.removeClass(element,"pulse");
+	$tw.utils.forceLayout(element);
+	$tw.utils.addClass(element,"pulse");
+};
+
+/*
+Cursor Click animation. Is useful to create videos
+This animation creates its own div element
+Options:
+clickEffect: CSS class name as string with no special formatting eg: clickEffect
+domNode: DOM node, that should contain the animated element. eg: rootElement
+event: event Object
+*/
+exports.clickAnimation = function(options) {
+	var options = options || {},
+		eventName = $tw.utils.convertEventName("animationEnd"),
+		element = document.createElement("div");
+
+	element.className = options.clickEffect || "";
+	element.style.top = options.event.clientY+"px";
+	element.style.left = options.event.clientX+"px";
+	options.domNode.appendChild(element);
+	// Event handler to remove the element if animation is finished
+	element.addEventListener(eventName,function() {
+		element.parentElement.removeChild(element);
+		}.bind(this),false);
+}
+
+})();
---
+++ wikilabs/click-effect/tiddlers/core/popup-tweaks.js
@@ -0,0 +1,62 @@
+/*\
+title: $:/core/modules/utils/dom/popup-tweaks.js
+type: application/javascript
+module-type: utils
+
+Module that creates a $tw.utils.Popup object prototype that manages popups in the browser
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+// Configuration tiddler, if click animation shuld be shown!
+var CONFIG_CLICK_ANIMATION = "$:/config/clickEffect";
+var Popup = require("$:/core/modules/utils/dom/popup.js").Popup;
+
+// CHANGE -  CHANGE -  CHANGE -  CHANGE -  CHANGE
+Popup.prototype.handleEvent = function(event) {
+	if(event.type === "click") {
+		// Find out what was clicked on
+		var info = this.popupInfo(event.target),
+			cancelLevel = info.popupLevel - 1;
+		// Don't remove the level that was clicked on if we clicked on a handle
+		if(info.isHandle) {
+			cancelLevel++;
+		}
+		// Cancel
+		this.cancel(cancelLevel);
+		// Show Click Animation
+		this.clickAnimation(event);
+	}
+};
+
+Popup.prototype.pulseElement = function(domNode) {
+	$tw.utils.pulseElement(domNode)
+};
+
+/*
+Animation Options: ... if configured
+clickEffect: CSS class name as string with no special formatting eg: clickEffect
+domNode: DOM node, that should contain the animated element. eg: rootElement
+event: event Object
+*/
+Popup.prototype.clickAnimation = function(event) {
+	var options = {};
+	options.event = event;
+	var clickEffect = $tw.wiki.getTiddlerText(CONFIG_CLICK_ANIMATION,"no").trim();
+	if (clickEffect === "clickEffect") {
+		options = {
+			clickEffect: "clickEffect",
+			domNode: this.rootElement,
+			event: event
+		}
+		$tw.utils.clickAnimation(options);
+	} else if (clickEffect === "pulse") {
+		this.pulseElement(event.target);
+	}
+};
+
+})();
