e868670bac31aafd9cb7680cbfeae3789ca8ed31
Mario Pietsch <pmariojo@gmail.com>
2022-11-09 14:06:40 +0100

    functionality is feature complete and works

--- wikilabs/palette-manager/tiddlers/widgets/edit-colour.js
+++ wikilabs/palette-manager/tiddlers/widgets/edit-colour.js
@@ -27,6 +27,9 @@ Set a field or index at a given tiddler to a colour
 	Render this widget into the DOM
 	*/
 	EditColourWidget.prototype.render = function(parent,nextSibling) {
+
+		// TODO use template instead of hardcoded
+
 		// Save the parent dom node
 		this.parentDomNode = parent;
 		// Compute our attributes
@@ -40,7 +43,6 @@ Set a field or index at a given tiddler to a colour
 		if (this.id) { this.inputDomNode.setAttribute("id", this.id); }
 		if (this.name) { this.inputDomNode.setAttribute("name", this.name); }
 		if (this.tooltip) { this.inputDomNode.setAttribute("title", this.tooltip); }
-		if (this.resolvedColour) { this.inputDomNode.setAttribute("resolvedColour", this.resolvedColour); }
 		if(this.isDisabled === "yes") {
 			this.inputDomNode.setAttribute("disabled",true);
 		}
@@ -61,61 +63,77 @@ Set a field or index at a given tiddler to a colour
 		this.renderChildren(this.inputDomNode,null);
 		this.domNodes.push(this.inputDomNode);
 	};
-	
-	EditColourWidget.prototype.initColour = function() {
-	}
-	
-	
-	EditColourWidget.prototype.getValue = function() {
-		var tiddler = this.wiki.getTiddler(this.title);
+
+	EditColourWidget.prototype.getValue = function(title) {
+		title = title || this.title;
+		var tiddler = this.wiki.getTiddler(title);
 		if(tiddler) {
 			if(this.index) {
-				this.colourValue = this.wiki.extractTiddlerDataItem(this.title,this.index,this.default);
-			} else {
+				this.colourValue = this.wiki.extractTiddlerDataItem(title,this.index,this.default);
+			} else if (this.field) {
 				this.colourValue = tiddler.getFieldString(this.field,this.default);
 			}
-			if (this.detectMacroFilter) {
-				if ((this.wiki.filterTiddlers(this.detectMacroFilter, this)[0]) && (this.resolvedColour)) {
-					this.value = this.validateRGB(this.resolvedColour);
-					return this.value;
-				}
-			}
-		} else {
-			this.colourValue = this.default;
-			this.setValue();
 		}
+		// normalize RGB values eg: #bbb
 		this.value = this.validateRGB(this.colourValue);
-		return this.colourValue;
+		return this.value;
 	};
-	
-	EditColourWidget.prototype.setValue = function() {
+
+	EditColourWidget.prototype.setValue = function(title, value) {
+		title = title || this.title;
+		value = value || this.colourValue;
 		if(this.index) {
-			this.wiki.setText(this.title,"",this.index,this.colourValue);
+			this.wiki.setText(title,"",this.index,value);
 		} else {
-			var tiddler = this.wiki.getTiddler(this.title),
+			var tiddler = this.wiki.getTiddler(title),
 				addition = {};
-			addition[this.field] = this.colourValue;
-			this.wiki.addTiddler(new $tw.Tiddler(this.wiki.getCreationFields(),{title: this.title},tiddler,addition,this.wiki.getModificationFields()));
+			addition[this.field] = value;
+			this.wiki.addTiddler(new $tw.Tiddler(this.wiki.getCreationFields(),{title:title},tiddler,addition,this.wiki.getModificationFields()));
 		}
 	};
 
 	EditColourWidget.prototype.handleInputEvent = function(event) {
 		this.colourValue = event.target.value;
 		if(this.liveUpdate === "yes") {
-			this.setValue();
+			if(this.inputActions) {
+				this.invokeActionString(this.inputActions,this,event,
+					{"tiddler": this.title, "field":this.field, "index":this.index, "value": this.colourValue});
+			} else {
+				this.setValue();
+			}
 		}
 	}
 
 	EditColourWidget.prototype.handleChangeEvent = function(event) {
 		this.colourValue = event.target.value;
-		this.setValue();
+		// this.setValue();
 		// Trigger actions
-		if(this.onChangeActions) {
-			this.invokeActionString(this.onChangeActions,this,event,{"colourValue": this.colourValue});
+		if(this.changeActions) {
+			this.invokeActionString(this.changeActions,this,event,
+				{"tiddler":this.title, "field":this.field, "index":this.index, "value":this.colourValue});
+		} else {
+			this.setValue();
 		}
 	};
-	
-	EditColourWidget.prototype.validateRGB = function(value) {
+
+	EditColourWidget.prototype.tempTiddlerName = function() {
+		return this.tempPrefix + this.title;
+	}
+
+	EditColourWidget.prototype.handleInitActions = function() {
+		if(this.initActions) {
+			this.invokeActionString(this.initActions,this,null, {"col-temp-tiddler": this.tempTiddlerName()});
+			return this.getValue(this.tempTiddlerName())
+		} else {
+			return this.getValue()
+		}
+	}
+
+	EditColourWidget.prototype.validateRGB = function(value, recurse) {
+		recurse = recurse || false
+//TODO get https://colorjs.io/ and create a custom colour picker
+//as shown at: https://stackoverflow.com/a/39399649
+
 		var x = value.trim().split("");
 		var newColour = [];
 		if ((x[0] === "#") && (x.length === 4)) {
@@ -123,6 +141,12 @@ Set a field or index at a given tiddler to a colour
 			$tw.utils.each(x, function(v){
 				(v === "#") ? newColour.push(v) : newColour.push(v+v);
 			})
+		} else if (x[0] != "#") {
+				if (recurse === false) {
+					return this.validateRGB(this.default, true); // use default value
+				} else {
+					return this.default;
+				}
 		} else {
 			return x.join("");
 		}
@@ -137,6 +161,7 @@ Set a field or index at a given tiddler to a colour
 		// According to MDN example: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/color
 		this.id = this.getAttribute("id","");			// Can be used together with <label> element
 		this.name = this.getAttribute("name","");		// Can be used together with <label> element
+		this.isDisabled = this.getAttribute("disabled","no");
 		this.tooltip = this.getAttribute("tooltip","");	// Show tooltip
 		this.liveUpdate = this.getAttribute("liveUpdate","yes");	// Continous update values ... for backwards compatibility
 		// TW specific
@@ -144,17 +169,24 @@ Set a field or index at a given tiddler to a colour
 		this.field = this.getAttribute("field","color");
 		this.index = this.getAttribute("index");
 		this.cClass = this.getAttribute("class","");
-		this.default = this.getAttribute("default");
-		this.detectMacroFilter = this.getAttribute("detectMacroFilter");
-		this.resolvedColour = this.validateRGB(this.getAttribute("resolvedColour"));
-		this.isDisabled = this.getAttribute("disabled","no");
-		this.onChangeActions = this.getAttribute("onChangeActions","");
-	
-		this.getValue();
+
+		this.default = this.getAttribute("default","#f00");
+		this.colourValue = this.default;
+
+		this.tempPrefix = this.getAttribute("tempPrefix","$:/temp/");
+
+		this.changeActions = this.getAttribute("$change");
+		this.inputActions = this.getAttribute("$input");
+		this.initActions = this.getAttribute("resolveColourActions");
+
+		this.template = this.getAttribute("template");
+
+		// Initialisation
+		this.handleInitActions();
 		// Make the child widgets
 		this.makeChildWidgets();
 	};
-	
+
 	/*
 	Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
 	*/
@@ -164,7 +196,7 @@ Set a field or index at a given tiddler to a colour
 			this.refreshSelf();
 			return true;
 		} else if(changedTiddlers[this.title]) {
-			this.inputDomNode.value = this.getValue();
+			this.inputDomNode.value = this.handleInitActions();
 			return this.refreshChildren(changedTiddlers);
 		} else {
 			return this.refreshChildren(changedTiddlers);
