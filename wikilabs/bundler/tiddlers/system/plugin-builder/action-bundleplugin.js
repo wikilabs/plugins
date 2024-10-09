60ebca198dea2d2ae18063bbb7e1eb78a937d7b1
Mario Pietsch <pmariojo@gmail.com>
2024-10-09 14:39:38 +0200

    bundler -- bundle plugin initial take

---
+++ wikilabs/bundler/tiddlers/system/plugin-builder/action-bundleplugin.js
@@ -0,0 +1,122 @@
+/*\
+title: $:/plugins/wikilabs/bundler/action-bundleplugin.js
+type: application/javascript
+module-type: widget
+
+Action widget to bundle an existing or a new plugin.
+
+\*/
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+var Widget = require("$:/core/modules/widgets/widget.js").widget;
+
+var BundlePluginWidget = function(parseTreeNode,options) {
+	this.initialise(parseTreeNode,options);
+};
+
+/*
+Inherit from the base widget class
+*/
+BundlePluginWidget.prototype = new Widget();
+
+/*
+Render this widget into the DOM
+*/
+BundlePluginWidget.prototype.render = function(parent,nextSibling) {
+	this.computeAttributes();
+	this.execute();
+};
+
+/*
+Compute the internal state of the widget
+*/
+BundlePluginWidget.prototype.execute = function() {
+	this.actionPlugin = this.getAttribute("$plugin");
+	this.actionInclude = this.getAttribute("$include");
+	this.actionExclude = this.getAttribute("$exclude");
+	this.actionRemove = this.getAttribute("$remove");
+	this.actionMode = this.getAttribute("$mode","log");
+};
+
+/*
+Refresh the widget by ensuring our attributes are up to date
+*/
+BundlePluginWidget.prototype.refresh = function(changedTiddlers) {
+	var changedAttributes = this.computeAttributes();
+	if(changedAttributes["$pluginname"] || changedAttributes["$filter"] || changedAttributes["$excludeFilter"] || changedAttributes["$mode"]) {
+		this.refreshSelf();
+		return true;
+	}
+	return this.refreshChildren(changedTiddlers);
+};
+
+/*
+Invoke the action associated with this widget
+*/
+BundlePluginWidget.prototype.invokeAction = function(triggeringWidget,event) {
+	event = event || {};
+	var tiddlers = $tw.wiki.filterTiddlers(this.actionInclude),
+		excluded = $tw.wiki.filterTiddlers(this.actionExclude) || [[]],
+		remove = $tw.wiki.filterTiddlers(this.actionRemove) || [[]],
+		self = this;
+
+	// Add tiddlers and remove added tiddlers from the store
+	// Warning!: This funcionality is able to "destroy" your plugin
+	function write() {
+		if (tiddlers && self.actionPlugin) {
+			$tw.utils.bundlePlugin(self.actionPlugin, tiddlers, excluded, remove, {incrementPatch:false});
+			log("dangerous");
+		}
+	};
+	// Allow the user to veryfy, that everything will be OK
+	function log(mode) {
+		mode = mode || "log";
+		var txtModified = (mode === "log") ? "should be modified." : (mode === "dangerous") ? "has been modified." : "unknown 'mode'";
+		var logFile = (mode === "dangerous") ? "bundle.plugin.log" : "bundle-plugin-verify";
+		var text = $tw.wiki.getTiddler(logFile)?.fields?.text || "";
+
+		text += "--------\n" + $tw.macros.now.run("YYYY-0MM-0DD 0hh:0mm:0ss-0XXX") + "\n\n";
+
+		text += "Plugin: [[" + self.actionPlugin + "]] " + txtModified + "\n\n";
+
+		if (tiddlers.length === 0) {
+			text += "* No 'include' tiddlers selected\n"
+		} else {
+			$tw.utils.each(tiddlers, function(title) {
+				text += "# add: [[" + title + "]]\n";
+			})
+		}
+		if (excluded.length > 0) {
+			text += "\n"
+			$tw.utils.each(excluded, function(title) {
+				text += "# excluded: [[" + title + "]]\n";
+			})
+		}
+
+		if (remove.length > 0) {
+			text += "\n"
+			$tw.utils.each(remove, function(title) {
+				text += "# remove: [[" + title + "]]\n";
+			})
+		}
+
+		text += "That's it\n\n";
+		$tw.wiki.addTiddler(new $tw.Tiddler(self.wiki.getCreationFields(),{title:logFile, text: text/*, type: "application/x-tiddler-dictionary"*/},self.wiki.getModificationFields()));
+		self.dispatchEvent({
+			type: "tm-navigate",
+			navigateTo: logFile
+		});
+	}
+
+	if (this.actionMode === "log") {
+		log();
+	} else if (this.actionMode === "dangerous") {
+		write();
+	}
+	return true; // Action was invoked
+};
+
+exports["action-bundleplugin"] = BundlePluginWidget;
---
+++ wikilabs/bundler/tiddlers/system/plugin-builder/bundleplugin.js
@@ -0,0 +1,95 @@
+/*\
+title: $:/plugins/wikilabs/bundler/bundleplugin.js
+type: application/javascript
+module-type: utils
+
+A quick and dirty way to pack up plugins within the browser.
+
+\*/
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+/*
+Build a plugin, and then delete any non-shadow payload tiddlers.
+options:
+	incrementMinor ... auto increment version minor
+	incrementPatch ... auto increment version patch
+	removePrefix ... plugin tiddler title prefix that should be removed
+	addPrefix ... new title prefix
+*/
+exports.bundlePlugin = function(title,additionalTiddlers,excludeTiddlers,removeTiddlers,options) {
+	options = options || {incrementMinor: false, incrementPatch: true};
+	additionalTiddlers = additionalTiddlers || [];
+	excludeTiddlers = excludeTiddlers || [];
+	removeTiddlers = removeTiddlers || [];
+	// Get the plugin tiddler
+	var pluginTiddler = $tw.wiki.getTiddler(title);
+	if(!pluginTiddler) {
+		throw "No such tiddler as " + title;
+	}
+	// Extract the JSON
+	var jsonPluginTiddler = $tw.utils.parseJSONSafe(pluginTiddler.fields.text,null);
+	if(!jsonPluginTiddler) {
+		throw "Cannot parse plugin tiddler " + title + "\n" + $tw.language.getString("Error/Caption") + ": " + e;
+	}
+	// Get the list of tiddlers
+	var tiddlers = Object.keys(jsonPluginTiddler.tiddlers);
+	// Add the additional tiddlers
+	$tw.utils.pushTop(tiddlers,additionalTiddlers);
+	// Remove any excluded tiddlers
+	for(var t=tiddlers.length-1; t>=0; t--) {
+		if((excludeTiddlers.indexOf(tiddlers[t]) !== -1) || (removeTiddlers.indexOf(tiddlers[t]) !== -1) ) {
+			tiddlers.splice(t,1);
+		}
+	}
+	// $tw.utils.each(tiddlers,function(title){
+	// 	if (options.removePrefix && options.addPrefix) {
+	// 		if (title.indexOf(options.removePrefix) === 0) {
+	// 			// 
+	// 			var a = 1;
+	// 		}
+	// 	}
+	// });
+	// Pack up the tiddlers into a block of JSON
+	var plugins = {};
+	$tw.utils.each(tiddlers,function(title) {
+		var tiddler = $tw.wiki.getTiddler(title),
+			fields = {};
+		$tw.utils.each(tiddler.fields,function (value,name) {
+			fields[name] = tiddler.getFieldString(name);
+		});
+		plugins[title] = fields;
+	});
+	// Retrieve and bump the version number
+	var pluginVersion = $tw.utils.parseVersion(pluginTiddler.getFieldString("version") || "0.0.0") || {
+			major: "0",
+			minor: "0",
+			patch: "0"
+		};
+	if (options.incrementMinor) {
+			pluginVersion.minor++;
+		}
+	if (options.incrementPatch) {
+			pluginVersion.patch++;
+		}
+	var version = pluginVersion.major + "." + pluginVersion.minor + "." + pluginVersion.patch;
+	if(pluginVersion.prerelease) {
+		version += "-" + pluginVersion.prerelease;
+	}
+	if(pluginVersion.build) {
+		version += "+" + pluginVersion.build;
+	}
+	// Save the plugin tiddler
+	$tw.wiki.addTiddler(new $tw.Tiddler(pluginTiddler,{text: JSON.stringify({tiddlers: plugins},null,4), version: version}));
+	// Delete any non-shadow constituent tiddlers
+	$tw.utils.each(tiddlers,function(title) {
+		if($tw.wiki.tiddlerExists(title)) {
+			$tw.wiki.deleteTiddler(title);
+		}
+	});
+	// Trigger an autosave
+	$tw.rootWidget.dispatchEvent({type: "tm-auto-save-wiki"});
+	// Return a heartwarming confirmation
+	return "Plugin " + title + " successfully saved";
+};
